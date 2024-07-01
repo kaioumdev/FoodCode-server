b6c8b46549d4bc480e1752042e901ce2bad9a2a2
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-02 04:03:15 +0600

    feat(createCarts): generate post method to create new carts

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     await client.connect();
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewCollection = client.db("bistroDB").collection("reviews");
+    const cartCollection = client.db("bistroDB").collection("carts");
 
     app.get("/menu", async(req, res) => {
         const result = await menuCollection.find().toArray();
@@ -38,6 +39,12 @@ async function run() {
     app.get("/review", async(req, res) => {
         const result = await reviewCollection.find().toArray();
         res.send(result);
+    })
+
+    app.post("/carts", async(req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
     })
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
