ecbcdfd6672c7a1767f1b8bfb442a2518e438f41
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-05 10:44:24 +0600

    feat(getAllUsers): create get method to get all users data

--- index.js
+++ index.js
@@ -32,7 +32,12 @@ async function run() {
     const reviewCollection = client.db("bistroDB").collection("reviews");
     const cartCollection = client.db("bistroDB").collection("carts");
 
-    //userCollection
+    //user related api
+    app.get("users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post("/users", async(req, res) => {
       const user = req.body;
       const query = {email: user.email}
