c52a6c2b474709b5cb6eea1d07d50dd27f5b7045
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-09 19:34:27 +0600

    feat(getSingleMenu):create get method to get single menu from database

--- index.js
+++ index.js
@@ -128,8 +128,11 @@ async function run() {
 
     app.get("/menu/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
+      console.log(id);
+      const query = {_id: new ObjectId(id)};
+      console.log(query);
       const result = await menuCollection.findOne(query);
+      console.log(result);
       res.send(result);
     })
 
