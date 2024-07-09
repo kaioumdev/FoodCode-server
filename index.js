8252c29c00e83dfc28961491f49a5c5cb2525649
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-09 14:27:58 +0600

    feat(singleMenu): get single menu item to implements get method

--- index.js
+++ index.js
@@ -126,6 +126,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
       const menuItem = req.body;
       const result = await menuCollection.insertOne(menuItem);
