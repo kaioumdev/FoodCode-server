5365c57435571a461600b31e7aae2bd7be415c75
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-01 17:36:00 +0600

    feat(updated): updated payment method

--- index.js
+++ index.js
@@ -39,14 +39,14 @@ async function run() {
     });
 
     const verifyToken = (req, res, next) => {
-      // console.log(req.headers.authorization);
+      console.log(req.headers.authorization);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: "Unauthorized Access token" });
       }
       const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
         if (error) {
-          return res.status(401).send({ message: "Forbidden Access token" });
+          return res.status(403).send({ message: "Forbidden Access token" });
         }
         req.decoded = decoded;
         next();
