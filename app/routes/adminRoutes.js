9bf336bc167b58f741d607572b228394e9a250da
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-25 16:02:29 +0600

    feat(adminRoutes): create adminRoutes to controll adminRouter function

---
+++ app/routes/adminRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const {
+  getAdminStats,
+  getOrderStats,
+} = require("../controllers/adminController");
+const verifyToken = require("../middlewares/authMiddleware");
+const verifyAdmin = require("../middlewares/adminMiddleware");
+const router = express.Router();
+
+router.get("/admin-stats", verifyToken, verifyAdmin, getAdminStats);
+router.get("/order-stats", getOrderStats);
+
+module.exports = router;
