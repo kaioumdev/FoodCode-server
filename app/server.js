84075dd1433ebcf95eca94ab5961e6512312c80b
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-25 16:04:59 +0600

    feat(server): create server.js file to run the whole code at server file

---
+++ app/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { connectDB } = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const menuRoutes = require("./routes/menuRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/menu", menuRoutes);
+app.use("/review", reviewRoutes);
+app.use("/carts", cartRoutes);
+app.use("/payments", paymentRoutes);
+app.use("/admin", adminRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Bistro World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
