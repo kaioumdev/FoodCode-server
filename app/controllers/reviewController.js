407a9b1e55ea5ce8d5dfbbdfb410f55d3d92184c
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-07 20:16:01 +0600

    feat(getReviews): updated getReviews method to using try catch block

--- app/controllers/reviewController.js
+++ app/controllers/reviewController.js
@@ -3,8 +3,13 @@ const { client } = require("../config/db");
 const reviewCollection = client.db("bistroDB").collection("reviews");
 
 const getReviews = async (req, res) => {
-  const result = await reviewCollection.find().toArray();
-  res.send(result);
+  try {
+    const result = await reviewCollection.find().toArray();
+    res.send(result);
+  } catch (error) {
+    console.error("Can not get reviews:", error);
+    res.status(500).send({ message: "Can not get reviews" });
+  }
 };
 
 module.exports = { getReviews };
