514e9f8bb4b0a8717229e099e40bddc9bf2d6652
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-22 20:02:04 +0600

    Bugs(server.js): remove old unnecessry server.js file & implements new cors policy to solve the errors

--- app/server.js
+++ app/server.js
@@ -21,20 +21,6 @@ app.use(cors({
   credentials: true
 }));
 
-// app.use(function (request, response, next) {
-//   response.header("Access-Control-Allow-Origin", "*");
-//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PATCH,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x - client - key, x - client - token, x - client - secret, Authorization");
-  next();
-});
-
 // ✅ Middleware
 app.use(express.json());
 
