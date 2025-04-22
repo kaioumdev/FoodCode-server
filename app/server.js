f15674ba1987912b0f11481b04b60effbf408b3b
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-22 17:03:43 +0600

    Bugs(server.js): update all server.js file to solve the cors-policy errors

--- app/server.js
+++ app/server.js
@@ -21,6 +21,12 @@ app.use(cors({
   credentials: true
 }));
 
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // ✅ Middleware
 app.use(express.json());
 
