e9222731766ef61bac1cd0ffa27e6ca7d060fd49
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-23 19:29:36 +0600

    (createJWT): clean createJWT file & remove console from createJWT file

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -9,7 +9,6 @@ const createJWT = (req, res) => {
     });
     res.send({ token });
   } catch (error) {
-    console.error("Failed to create jwtToken:", error);
     res.status(500).send({ message: "Failed to create jwtToken" });
   }
 };
